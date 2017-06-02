a8d6e5a0972e483b81d5f8905a45191af2bdc902
GiangH <sirgiang94@gmail.com>
2017-06-02 08:22:30 +0700

    first commit

---
+++ dist/adm-structures-base.js
@@ -0,0 +1,174 @@
+/* *
+ * The MIT License
+ *
+ * Copyright (c) 2015, Sebastian Sdorra
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+'use strict';
+
+angular.module('adf.structures.base', ['adf'])
+  .config(["dashboardProvider", function(dashboardProvider){
+
+    dashboardProvider
+      .structure('12', {
+        rows: [
+            {
+                styleClass: 'layout-column',
+                columns:[
+                    {
+                        styleClass: 'flex-100'
+                    }
+                ]
+            }
+        ]
+      })
+      .structure('6-6', {
+        rows: [{
+          styleClass: 'layout-row',
+          columns: [{
+            styleClass: 'flex-50'
+          }, {
+            styleClass: 'flex-50'
+          }]
+        }]
+      })
+      .structure('4-8', {
+        rows: [{
+          styleClass: 'layout-row layout-align-space-between-center',
+          columns: [{
+            styleClass: 'flex-33',
+            widgets: []
+          }, {
+            styleClass: 'flex-66',
+            widgets: []
+          }]
+        }]
+      })
+      .structure('4-4-4', {
+       rows:[
+         {
+           styleClass: 'layout-row',
+             columns:[
+               {
+                 styleClass: 'flex-33'
+               }
+               ,{
+                 styleClass: 'flex-33'
+               }
+               ,{
+                 styleClass: 'flex-33'
+               }
+             ]
+         }
+       ]
+      })
+        .structure('3-3-3-3', {
+            rows:[
+              {
+                styleClass: 'layout-row',
+                columns:[
+                  {
+                    styleClass: 'flex-25'
+                  }
+                  ,{
+                    styleClass: 'flex-25'
+                  }
+                  ,{
+                    styleClass: 'flex-25'
+                  }
+                  ,{
+                    styleClass: 'flex-25'
+                  }
+                ]
+              }
+            ]
+        })
+      .structure('12/4-4-4', {
+        rows: [{
+          columns: [{
+            styleClass: 'flex-100'
+          }]
+        }, {
+          styleClass: 'layout-row layout-align-space-between-center',
+          columns: [{
+            styleClass: 'flex-33'
+          }, {
+            styleClass: 'flex-33'
+          }, {
+            styleClass: 'flex-33'
+          }]
+        }]
+      })
+      .structure('12/6-6', {
+        rows: [{
+          columns: [{
+            styleClass: 'flex-100'
+          }]
+        }, {
+          styleClass: 'layout-row',
+          columns: [{
+            styleClass: 'flex-50'
+          }, {
+            styleClass: 'flex-50'
+          }]
+        }]
+      })
+      .structure('12/6-6/12', {
+        rows: [{
+          columns: [{
+            styleClass: 'flex-100'
+          }]
+        }, {
+          styleClass: 'layout-row',
+          columns: [{
+            styleClass: 'flex-50'
+          }, {
+            styleClass: 'flex-50'
+          }]
+        }, {
+          columns: [{
+            styleClass: 'flex-100'
+          }]
+        }]
+      })
+      .structure('3-9 (12/6-6)', {
+        rows: [{
+          styleClass: 'layout-row',
+          columns: [{
+            styleClass: 'flex-25'
+          }, {
+            styleClass: 'flex-75',
+            rows: [{
+              columns: [{
+                styleClass: 'flex-100'
+              }]
+            }, {
+              styleClass: 'layout-row',
+              columns: [{
+                styleClass: 'flex-50'
+              }, {
+                styleClass: 'flex-50'
+              }]
+            }]
+          }]
+        }]
+      });
+
+  }]);
